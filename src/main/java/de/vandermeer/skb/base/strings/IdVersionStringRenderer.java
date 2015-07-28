1411e6b00eb6b2ec5ed4e769fd3fcdf70788f40f
vdmeer <sven@vandermeer.de>
2015-07-28 16:43:40 +0100

    added writers and string renderers

---
+++ src/main/java/de/vandermeer/skb/base/strings/IdVersionStringRenderer.java
@@ -0,0 +1,48 @@
+/* Copyright 2014 Sven van der Meer <vdmeer.sven@mykolab.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.vandermeer.skb.base.strings;
+
+import java.util.Locale;
+
+import org.stringtemplate.v4.AttributeRenderer;
+
+/**
+ * An ST string renderer {@link IdVersionString}.
+ * Register this renderer for the class {@link IdVersionString}.
+ * Formats are: "id" the id part of the string and all formats from {@link VersionStringRenderer}.
+ * If any problem occurred, the returned string will be "###";
+ *
+ * @author     Sven van der Meer &lt;vdmeer.sven@mykolab.com&gt;
+ * @version    v0.0.9-SNAPSHOT build 150727 (27-Jul-15) for Java 1.8
+ * @since      v0.0.8
+ */
+public class IdVersionStringRenderer implements AttributeRenderer {
+
+	@Override
+	public String toString(Object o, String format, Locale locale) {
+		if(o instanceof IdVersionString){
+			if("id".equals(format)){
+				return ((IdVersionString) o).getID();
+			}
+			if("version".equals(format) || "version-major".equals(format) || "version-minor".equals(format) || "version-patch".equals(format)){
+				VersionStringRenderer vsr = new VersionStringRenderer();
+				return vsr.toString(((IdVersionString) o).getVersion(), format, locale);
+			}
+		}
+
+		return "###";
+	}
+}
---
+++ src/main/java/de/vandermeer/skb/base/strings/VersionStringRenderer.java
@@ -0,0 +1,53 @@
+/* Copyright 2014 Sven van der Meer <vdmeer.sven@mykolab.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.vandermeer.skb.base.strings;
+
+import java.util.Locale;
+
+import org.stringtemplate.v4.AttributeRenderer;
+
+/**
+ * An ST string renderer {@link VersionString}.
+ * Register this renderer for the class {@link VersionString}.
+ * Formats are: "version" for the full version (same as original toString()), "version-major" for major part of version, "version-minor" for minor part of version, "version-patch" for patch part of version.
+ * If any problem occurred, the returned string will be "###";
+ *
+ * @author     Sven van der Meer &lt;vdmeer.sven@mykolab.com&gt;
+ * @version    v0.0.9-SNAPSHOT build 150727 (27-Jul-15) for Java 1.8
+ * @since      v0.0.8
+ */
+public class VersionStringRenderer implements AttributeRenderer {
+
+	@Override
+	public String toString(Object o, String format, Locale locale) {
+		if(o instanceof VersionString){
+			if("version".equals(format)){
+				return o.toString();
+			}
+			if("version-major".equals(format)){
+				return ((VersionString) o).getMajor().toString();
+			}
+			if("version-minor".equals(format)){
+				return ((VersionString) o).getMinor().toString();
+			}
+			if("version-patch".equals(format)){
+				return ((VersionString) o).getPatch().toString();
+			}
+		}
+		return "###";
+	}
+
+}
